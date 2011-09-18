492cb12c39446361fd77d0227306015718a35a4e
ve7fet <ve7fet@6329ae7b-5bdf-5775-36f2-7d60a0273684>
2011-09-18 05:18:21 +0000

    This branch is no longer required, now that we've moved the /debian folder in to the main code base.

--- branches/trunk-debian/wp/update.h
+++
@@ -1,6 +0,0 @@
-int set_dirty_context(int record, int client);
-int clear_dirty_context(int record, int client);
-int find_dirty_context(int client);
-int count_dirty_context(int client);
-void broadcast_dirty(int record, int except);
-void set_vector_when_nodirty(int client);
