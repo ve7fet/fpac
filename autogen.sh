7889dcb47d47b5388dee4c61559d0a9c2f52fa26
Bernard, f6bvp <f6bvp@free.fr>
2018-01-18 20:04:23 +0100

    New autogen.sh will create missing AX.25 library links (with help from Tom SP2L)

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,38 @@
 #!/bin/sh
+# VE7FET location
+LIBAX25DIR=/usr/local/lib/
+# distro location
+LIBAXDIR=/usr/lib/
+# AX.25 libraries & links
+LIBAX25SO=libax25.so
+LIBAX25IO=libax25io.so
+VER=.1.0.0
+DIR=$PWD
+# Check AX.25 libraries directory location
+if [ -f $LIBAXDIR/$LIBAX25SO$VER ] ; then
+ echo "AX.25 libraries are in $LIBAXDIR"
 
+# If required links do not exist lets create
+ cd $LIBAXDIR
+ if [ ! -f $LIBAX25SO ] ; then
+  ln -s $LIBAX25SO$VER $LIBAX25SO
+ echo "Created missing AX.25 library link $LIBAX25SO"
+ fi
+ if [ ! -f $LIBAXDIR/$LIBAX25IO ] ; then
+  ln -s $LIBAX25IO$VER $LIBAX25IO
+ echo "Created missing AX.25 library link $LIBAX25IO"
+ fi
+
+else
+ echo "Looking if AX.25 libraries are in $LIBAX25DIR" 
+ if [ -f $LIBAX25DIR/$LIBAX25SO$VER ] ; then
+ echo "OK !"
+ else
+ echo "AX.25 libraries are not installed in this system !" 
+ fi
+fi
+
+cd $DIR
 
 echo "---removing generated files---"
 # products from ./configure and make
