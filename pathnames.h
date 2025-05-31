280f1e81b935e58ef08a09469fe455de944c3936
Bernard <f6bvp@free.fr>
2025-05-31 18:16:12 +0200

    Add files via upload

---
+++ pathnames.h
@@ -0,0 +1,34 @@
+/*From FPAC sources*/
+
+#define FPACCONF        	"/usr/local/etc/ax25/fpac.conf"
+#define FPACNODES       	"/usr/local/etc/ax25/fpac.nodes"
+#define FPACROUTES      	"/usr/local/etc/ax25/fpac.routes"
+#define FPACDNIC        	"/usr/local/etc/ax25/fpac.dnic"
+#define FPAC_INFO_FILE  	"/usr/local/etc/ax25/fpac.info"
+#define FPAC_HELLO_FILE 	"/usr/local/etc/ax25/fpac.hello"
+#define FPAC_HELP_DIR   	"/usr/local/var/ax25/fpac/help/"
+#define FPACWP 			"/usr/local/var/ax25/fpac/fpacwp3.dat"
+
+#define FLEX_GT_FILE  		"/usr/local/var/ax25/flex/gateways"
+#define FLEX_DST_FILE 		"/usr/local/var/ax25/flex/destinations"
+
+#define DATA_MHEARD_FILE        "/usr/local/var/ax25/mheard/mheard.dat"
+
+#define DATA_NODE_LOGIN_FILE	"/usr/local/var/ax25/fpac/loggedin"
+
+#define FLEXD_CONF_FILE 	"/usr/local/etc/ax25/flexd.conf"
+#define FLEXD_TEMP_PATH 	"/usr/local/var/ax25/flex/"
+#define AX_ROUTES_FILE 		"/usr/local/etc/ax25/flexd.routes"
+
+#define CONF_RSPORTS_FILE 	"/usr/local/etc/ax25/rsports"
+
+#define CONF_AXPORTS_FILE       "/usr/local/etc/ax25/axports"
+
+#define CONF_APRSLIST_FILE 	"/usr/local/etc/ax25/aprslist.conf"
+#define DATA_APRSLIST_FILE 	"/usr/local/var/ax25/aprs.dat"
+
+#define STATLCK 		"/usr/local/var/ax25/fpac/fpacstat.lck"
+#define STATDAT 		"/usr/local/var/ax25/fpac/fpacstat.dat"
+#define STATDAY 		"/usr/local/var/ax25/fpac/fpacstat.day"
+#define STATPID 		"/usr/local/var/ax25/fpac/fpacstat.pid"
+
